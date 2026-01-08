780ee366d2a832c735a8ea9b499e960945908a26
tommimarcpricreykertzm-a11y <tommimarcpricreykertzm@gmail.com>
2026-01-08 18:54:29 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Apex Global Mining Group</title>
+  </head>
+  <body>
+    <div id="根"></div>
+    
+    <script type="module" src="/index.tsx"></script>
+  </body>
+</html>
